295d9d1ea855e2a9c1ba620d6dd7274df40cea21
Julius <juliusshrestha29@gmail.com>
2023-02-27 08:57:07 +0545

    email service WIP

--- services/email.service.js
+++ services/email.service.js
@@ -1,5 +1,22 @@
 function sendSignupEmail({ sender, receiver }) {}
 
-function sendForgotPasswordEmail(sender, receiver) {}
+async function sendForgotPasswordEmail(sender, receiver) {
+    const message = `You are receiving this email because you have requested the reset of a password, Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: admin.email,
+      subject: "Password reset token",
+      message,
+    });
+    res.status(200).json({
+      success: true,
+      data: "Email sent",
+    });
+  } catch (err) {
+    console.log(err);
+    return next(new ErrorResponse("Email could not be sent", 500));
+  }
+}
 
 module.exports = { sendSignupEmail, sendForgotPasswordEmail };
