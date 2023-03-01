5765d50a694c35895fb4fe4f8e4020f967e1ccaf
Julius <juliusshrestha29@gmail.com>
2023-03-01 12:35:50 +0545

    authService retrive new password field

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,14 +1,14 @@
 const { PrismaClient, Prisma } = require("@prisma/client");
 const { OAuth2Client } = require("google-auth-library");
 const {
-    encryptPassword,
-    generatePassword,
-    comparePassword,
+	encryptPassword,
+	generatePassword,
+	comparePassword,
 } = require("../utils/passwordGenerator");
 const jwtService = require("./jwt.service");
 const emailService = require("./email.service");
 const config = require("../utils/config");
-const {USER_STATUS } = require("../constants/app.constants");
+const { USER_STATUS } = require("../constants/app.constants");
 const { v4: uuidv4 } = require("uuid");
 const ApiError = require("../utils/errorHandler");
 const status = require("http-status");
@@ -26,15 +26,15 @@ const client = new OAuth2Client(googleClientId, googleClientSecret);
  * @returns
  */
 async function verifyGoogleToken(token) {
-    try {
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: googleClientId,
-        });
-        return { payload: ticket.getPayload() };
-    } catch (error) {
-        return { error: "Invalid user detected. Please try again" };
-    }
+	try {
+		const ticket = await client.verifyIdToken({
+			idToken: token,
+			audience: googleClientId,
+		});
+		return { payload: ticket.getPayload() };
+	} catch (error) {
+		return { error: "Invalid user detected. Please try again" };
+	}
 }
 
 /**
@@ -43,80 +43,80 @@ async function verifyGoogleToken(token) {
  * @returns
  */
 async function googleSignup(cred) {
-    try {
-        const verificationResponse = await verifyGoogleToken(cred);
+	try {
+		const verificationResponse = await verifyGoogleToken(cred);
 
-        if (verificationResponse.error) {
-            throw new Error(verificationResponse.error);
-        }
+		if (verificationResponse.error) {
+			throw new Error(verificationResponse.error);
+		}
 
-        const profile = verificationResponse?.payload;
+		const profile = verificationResponse?.payload;
 
-        const newUser = await prisma.tblUser.create({
-            data: {
-                firstName: profile.given_name,
-                lastName: profile.given_name,
-                role: ROLES.user,
-                userStatus: USER_STATUS.active,
-            },
-            credential: {
-                create: [{ email: profile.email }],
-            },
-            include: {
-                credential: true,
-            },
-        });
-        // create email with new user's credentials
-        return {
-            user: newUser,
-            token: jwtService.createToken(newUser),
-        };
-    } catch (error) {
-        throw error;
-    }
+		const newUser = await prisma.tblUser.create({
+			data: {
+				firstName: profile.given_name,
+				lastName: profile.given_name,
+				userStatus: USER_STATUS.active,
+			},
+			credential: {
+				create: [{ email: profile.email }],
+			},
+			include: {
+				tblCredential: true,
+			},
+		});
+		// create email with new user's credentials
+		return {
+			user: newUser,
+			token: jwtService.createToken(newUser),
+		};
+	} catch (error) {
+		throw error;
+	}
 }
 /**
  * service to create new user
- * @param {{email: String, password: String}} userDetails
+ * @param {{ email: String, password: String}} userDetails
  */
 async function signup(userDetails) {
-    try {
-        // add user logic
-        const existingUser = await prisma.tblCredential.findUnique({
-            where: { email: userDetails.email },
-            include: {
-                user: true,
-            },
-        });
-        debug(existingUser, userDetails);
-        if (existingUser) {
-            throw new ApiError({
-                message:
-                    "Email already exists. Please try again with a new email address.",
-                statusCode: status.CONFLICT,
-            });
-        }
+	try {
+		// add user logic
 
-        const newUser = await prisma.tblUser.create({
-            data: {
-                userId: uuidv4(),
-                credential: {
-                    create: {
-                        password: encryptPassword(userDetails.password),
-                        email: userDetails.email,
-                    },
-                },
-            },
-            include: { credential: true },
-        });
-        // create email with new user's credentials
-        return {
-            user: newUser,
-            token: jwtService.createToken(newUser),
-        };
-    } catch (error) {
-        throw error;
-    }
+		const existingUser = await prisma.tblCredential.findUnique({
+			where: { email: userDetails.email },
+			include: {
+				user: true,
+			},
+		});
+		debug(existingUser, userDetails);
+		if (existingUser) {
+			throw new ApiError({
+				message:
+					"Email already exists. Please try again with a new email address.",
+				statusCode: status.CONFLICT,
+			});
+		}
+
+		const newUser = await prisma.tblUser.create({
+			data: {
+				userId: uuidv4(),
+				tblCredential: {
+					create: {
+						password: encryptPassword(userDetails.password),
+						email: userDetails.email,
+					},
+				},
+			},
+			include: { tblCredential: true },
+		});
+		// create email with new user's credentials
+		return {
+			user: newUser,
+			token: jwtService.createToken(newUser),
+		};
+	} catch (error) {
+		throw error;
+	}
 }
 
 /**
@@ -125,33 +125,33 @@ async function signup(userDetails) {
  * @returns
  */
 async function googleSignin(credentials) {
-    try {
-        const verificationResponse = await verifyGoogleToken(credentials);
-        if (verificationResponse.error) {
-            throw new Error(verificationResponse.error);
-        }
-        const profile = verificationResponse?.payload;
-        const existingUser = await prisma.tblUser.findUnique({
-            where: { credential: { email: profile.email } },
-            include: {
-                credential: {
-                    select: { email: true, password: true, createdAt: true },
-                },
-            },
-        });
-        if (!existingUser) {
-            throw new ApiError({
-                message: "User does not exist",
-                statusCode: status.NOT_FOUND,
-            });
-        }
-        return {
-            user: existingUser,
-            token: jwtService.createToken(existingUser),
-        };
-    } catch (error) {
-        throw error;
-    }
+	try {
+		const verificationResponse = await verifyGoogleToken(credentials);
+		if (verificationResponse.error) {
+			throw new Error(verificationResponse.error);
+		}
+		const profile = verificationResponse?.payload;
+		const existingUser = await prisma.tblUser.findUnique({
+			where: { credential: { email: profile.email } },
+			include: {
+				credential: {
+					select: { email: true, password: true, createdAt: true },
+				},
+			},
+		});
+		if (!existingUser) {
+			throw new ApiError({
+				message: "User does not exist",
+				statusCode: status.NOT_FOUND,
+			});
+		}
+		return {
+			user: existingUser,
+			token: jwtService.createToken(existingUser),
+		};
+	} catch (error) {
+		throw error;
+	}
 }
 
 /**
@@ -160,37 +160,37 @@ async function googleSignin(credentials) {
  * @returns
  */
 async function signin(userDetails) {
-    try {
-        const existingUser = await prisma.tblCredential.findUnique({
-            where: { email: userDetails.email },
-            include: {
-                user: true,
-            },
-        });
-        if (!existingUser) {
-            throw new ApiError({
-                message: "User not found",
-                statusCode: status.NOT_FOUND,
-            });
-        }
-        if (comparePassword(userDetails.password, existingUser.password)) {
-            //generate jwt and login
-            return {
-                user: {
-                    ...existingUser.user,
-                    createdAt: existingUser.createdAt,
-                },
-                token: jwtService.createToken(existingUser),
-            };
-        } else {
-            throw new ApiError({
-                message: "Password does not match",
-                statusCode: status.UNAUTHORIZED,
-            });
-        }
-    } catch (error) {
-        throw error;
-    }
+	try {
+		const existingUser = await prisma.tblCredential.findUnique({
+			where: { email: userDetails.email },
+			include: {
+				user: true,
+			},
+		});
+		if (!existingUser) {
+			throw new ApiError({
+				message: "User not found",
+				statusCode: status.NOT_FOUND,
+			});
+		}
+		if (comparePassword(userDetails.password, existingUser.password)) {
+			//generate jwt and login
+			return {
+				user: {
+					...existingUser.user,
+					createdAt: existingUser.createdAt,
+				},
+				token: jwtService.createToken(existingUser),
+			};
+		} else {
+			throw new ApiError({
+				message: "Password does not match",
+				statusCode: status.UNAUTHORIZED,
+			});
+		}
+	} catch (error) {
+		throw error;
+	}
 }
 
 /**
@@ -199,21 +199,31 @@ async function signin(userDetails) {
  * @returns
  */
 async function forgotPassword(userDetails) {
-    try {
-        //generate random Password
-        const password = generatePassword();
-        const updatedUser = await prisma.tblCredential.update({
-            where: {
-                userId: userDetails.id,
-            },
-            data: {
-                password: encryptPassword(password),
-            },
-        });
-        return { password };
-    } catch (error) {
-        throw error;
-    }
+	try {
+		//generate random Password
+		const password = generatePassword();
+		const existingUser = await prisma.tblUser.findUnique({
+			where: {
+				userId: userDetails.userId,
+			},
+			include: { tblCredential: true },
+		});
+
+		const updatedUser = await prisma.tblCredential.update({
+			where: {
+				userId: userDetails.userId,
+			},
+			data: {
+				password: encryptPassword(userDetails.newPassword),
+			},
+		});
+		email = existingUser.tblCredential.email;
+
+		return { password, email };
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
 }
 
 /**
@@ -222,38 +232,38 @@ async function forgotPassword(userDetails) {
  * @returns
  */
 async function changePassword(userDetails) {
-    try {
-        const existingUser = await prisma.tblCredential.findUnique({
-            where: { userId: userDetails.userId },
-        });
+	try {
+		const existingUser = await prisma.tblCredential.findUnique({
+			where: { userId: userDetails.userId },
+		});
 
-        if (!comparePassword(userDetails.password, existingUser.password)) {
-            throw new ApiError({
-                message: "old password does not match",
-                statusCode: status.UNAUTHORIZED,
-            });
-        }
+		if (!comparePassword(userDetails.oldPassword, existingUser.password)) {
+			throw new ApiError({
+				message: "old password does not match",
+				statusCode: status.UNAUTHORIZED,
+			});
+		}
 
-        const updatedUser = await prisma.tblCredential.update({
-            where: {
-                userId: userDetails.userId,
-            },
-            data: {
-                password: encryptPassword(userDetails.newPassword),
-            },
-        });
+		const updatedUser = await prisma.tblCredential.update({
+			where: {
+				userId: userDetails.userId,
+			},
+			data: {
+				password: encryptPassword(userDetails.newPassword),
+			},
+		});
 
-        return updatedUser;
-    } catch (error) {
-        throw error;
-    }
+		return updatedUser;
+	} catch (error) {
+		throw error;
+	}
 }
 
 module.exports = {
-    signin,
-    changePassword,
-    signup,
-    forgotPassword,
-    googleSignin,
-    googleSignup,
+	signin,
+	changePassword,
+	signup,
+	forgotPassword,
+	googleSignin,
+	googleSignup,
 };
