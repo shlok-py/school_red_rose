f9a7f05b742707d2c38921e0298d682648dfbcd9
Julius <juliusshrestha29@gmail.com>
2023-03-02 21:36:52 +0545

    routes updated

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,10 @@
 const Router = require("@koa/router");
 const {
-    signup,
-    signin,
-    changePassword,
-    forgotPassword,
+	signup,
+	signin,
+	changePassword,
+	forgotPassword,
+	userIDPassword,
 } = require("../controllers/auth.controller");
 const schemaValidate = require("../utils/schema.validation");
 const authSchema = require("../utils/interfaces/auth.interface");
@@ -13,14 +14,19 @@ const router = new Router();
 router.post("/register", schemaValidate(authSchema.signUpSchema), signup);
 router.post("/login", schemaValidate(authSchema.signInSchema), signin);
 router.patch(
-    "/change-password",
-    schemaValidate(authSchema.changePasswordSchema),
-    changePassword
+	"/change-password",
+	schemaValidate(authSchema.changePasswordSchema),
+	changePassword
 );
 router.post(
-    "/forgot-password",
-    schemaValidate(authSchema.changePasswordSchema),
-    forgotPassword
+	"/forgot-password",
+	schemaValidate(authSchema.changePasswordSchema),
+	forgotPassword
+);
+router.post(
+	"/get-email",
+	schemaValidate(authSchema.getEmailSchema),
+	userIDPassword
 );
 
 module.exports = router.routes();
