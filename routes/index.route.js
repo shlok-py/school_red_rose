053080e7b41c9cc01cd0deea8c288ba512b84e8a
Julius <juliusshrestha29@gmail.com>
2023-03-02 21:37:45 +0545

    minor fix

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,11 +3,9 @@ const Router = require("@koa/router");
 const componentsRoute = require("./components.route");
 const userRoute = require("./user.route");
 
-
 const router = new Router();
 router.use("/auth", authRoute);
 router.use("/user", userRoute);
 router.use("/comp", componentsRoute);
-
 
 module.exports = router.routes();
