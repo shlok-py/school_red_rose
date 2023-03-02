02c374745530000ffe04ff5b45ca9fc7f552233f
Julius <juliusshrestha29@gmail.com>
2023-03-02 21:39:02 +0545

    getEmailSchema added

--- utils/interfaces/auth.interface.js
+++ utils/interfaces/auth.interface.js
@@ -1,48 +1,56 @@
 const Joi = require("joi");
 
 const signInSchema = {
-    params: {},
-    query: {},
-    body: Joi.object().keys({
-        credential: Joi.any().optional(),
-        email: Joi.string().email().required(),
-        password: Joi.string().required(),
-    }),
+	params: {},
+	query: {},
+	body: Joi.object().keys({
+		credential: Joi.any().optional(),
+		email: Joi.string().email().required(),
+		password: Joi.string().required(),
+	}),
 };
 
 const signUpSchema = {
-    params: {},
-    query: {},
-    body: Joi.object()
-        .keys({
-            credential: Joi.any().optional(),
-            email: Joi.string().email().required(),
-            password: Joi.string().required(),
-        })
-        .required(),
+	params: {},
+	query: {},
+	body: Joi.object()
+		.keys({
+			credential: Joi.any().optional(),
+			email: Joi.string().email().required(),
+			password: Joi.string().required(),
+		})
+		.required(),
 };
 
 const forgotPasswordSchema = {
-    params: {},
-    query: {},
-    body: Joi.object().keys({
-        email: Joi.string().email().required(),
-    }),
+	params: {},
+	query: {},
+	body: Joi.object().keys({
+		email: Joi.string().email().required(),
+	}),
 };
 
 const changePasswordSchema = {
-    params: {},
-    query: {},
-    body: Joi.object().keys({
-        userId: Joi.string().required().guid(),
-        oldPassword: Joi.string().required(),
-        newPassword: Joi.string().required(),
-    }),
+	params: {},
+	query: {},
+	body: Joi.object().keys({
+		userId: Joi.string().required().guid(),
+		oldPassword: Joi.string().required(),
+		newPassword: Joi.string().required(),
+	}),
+};
+const getEmailSchema = {
+	params: {},
+	query: {},
+	body: Joi.object().keys({
+		email: Joi.string().required(),
+	}),
 };
 
 module.exports = {
-    signInSchema,
-    signUpSchema,
-    forgotPasswordSchema,
-    changePasswordSchema,
+	signInSchema,
+	signUpSchema,
+	forgotPasswordSchema,
+	changePasswordSchema,
+	getEmailSchema,
 };
