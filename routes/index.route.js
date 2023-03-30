6917862134e5102d470aa6d7007f81a127309647
Julius <juliusshrestha29@gmail.com>
2023-03-30 15:53:40 +0545

    album and photo route added

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,10 +2,14 @@ const authRoute = require("./auth.route");
 const Router = require("@koa/router");
 const componentsRoute = require("./components.route");
 const userRoute = require("./user.route");
+const albumRoute = require("./album.route");
+const photoRoute = require("./photo.route");
 
 const router = new Router();
 router.use("/auth", authRoute);
 router.use("/user", userRoute);
 router.use("/comp", componentsRoute);
+router.use("/album", albumRoute);
+router.use("/photo", photoRoute);
 
 module.exports = router.routes();
