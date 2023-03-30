5d613153b787709d7a7cfd084447c4aeeb8c6fa4
Julius <juliusshrestha29@gmail.com>
2023-03-30 15:54:34 +0545

    token expirey added

--- services/jwt.service.js
+++ services/jwt.service.js
@@ -1,20 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const TOKEN_SECRET = "12312323";
-const accessTokenExipry = "1h";
+const accessTokenExipry = "2h";
 /**
  * create a jwt
  * @param {{}} data
  * @returns {String} token
  */
 function createToken(data) {
-    return jwt.sign(
-        {
-            data: data,
-        },
-        TOKEN_SECRET,
-        { expiresIn: accessTokenExipry }
-    );
+	return jwt.sign(
+		{
+			data: data,
+		},
+		TOKEN_SECRET,
+		process.env.JWT_EXPIRY && { expiresIn: process.env.JWT_EXPIRY }
+	);
 }
 
 /**
@@ -23,12 +23,12 @@ function createToken(data) {
  * @returns
  */
 function matchToken(token) {
-    try {
-        const decoded = jwt.verify(token, TOKEN_SECRET);
-        return decoded;
-    } catch (err) {
-        throw err;
-    }
+	try {
+		const decoded = jwt.verify(token, TOKEN_SECRET);
+		return decoded;
+	} catch (err) {
+		throw err;
+	}
 }
 
 module.exports = { createToken, matchToken };
